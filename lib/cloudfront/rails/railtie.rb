b3914490965fa39680cfdefe83a66319db071268
Tore Eschliman <tore.eschliman@instacart.com>
2018-12-17 13:17:57 -0500

    this doesn't affect anywhere the module has already been included as written, so check for that and patch Rack::Request instead if so

--- lib/cloudfront/rails/railtie.rb
+++ lib/cloudfront/rails/railtie.rb
@@ -8,7 +8,7 @@ module Cloudfront
         end
       end
 
-      if ::Rails.version.start_with? '4.'
+      if ::Rails.version.start_with? '4.' || Rack::Request.include?(Rack::Request::Helpers)
         Rack::Request.prepend CheckTrustedProxies
       else
         Rack::Request::Helpers.prepend CheckTrustedProxies
